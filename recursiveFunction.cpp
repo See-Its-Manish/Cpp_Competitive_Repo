c4fad4c74d5fa7f4fafff246ca8b1dadef0327ba
See-Its-Manish <seeitsmanish@gmail.com>
2020-06-19 13:59:36 +0530

    Done a basic program of Recursion

---
+++ recursiveFunction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void printMyName(int n){		// Recursive Function
+	cout<<"MANISH"<<endl;
+	if(n==0) return;
+	printMyName(n-1);		// Call To ItSelf
+
+}
+
+int main(){
+	printMyName(10);
+	return 0;
+}
+
+
+
